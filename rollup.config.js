ab5370cea26366130599ee2f93b4d5a9acd23201
fixiabis <fixiabis@gmail.com>
2021-06-12 12:14:54 +0800

    瑣事: 增加設定

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from 'rollup-plugin-typescript2';
+
+let defaults = { compilerOptions: { declaration: true } };
+let hideDeclaration = { compilerOptions: { declaration: false } };
+
+export default [
+  {
+    input: './src/index.ts',
+
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        tsconfigDefaults: defaults,
+      }),
+    ],
+
+    output: {
+      file: 'lib/index.js',
+      format: 'cjs',
+      indent: true,
+    },
+  },
+  {
+    input: './src/index.ts',
+
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        tsconfigDefaults: hideDeclaration,
+      }),
+    ],
+
+    output: {
+      file: 'dist/relati.js',
+      format: 'umd',
+      name: 'relati',
+      indent: true,
+    },
+  },
+];
