e62d193f92f4b619c6e1e063291162dc7368bec2
fixiabis <fixiabis@gmail.com>
2021-06-27 09:50:39 +0800

    功能: 每個格子的標記狀態

--- src/Board/SquareOfBoard.ts
+++ src/Board/SquareOfBoard.ts
@@ -1,8 +1,11 @@
 import Board from './Board';
 
+type StateOfMark = Record<string, any>;
+
 class SquareOfBoard {
   public readonly x: number;
   public readonly y: number;
+  public readonly stateOfMark: StateOfMark = {};
   public mark: Mark | ' ' = ' ';
 
   constructor(public readonly coordinate: Coordinate, public readonly board: Board) {
