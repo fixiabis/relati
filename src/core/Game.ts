d11e744534c6a372f42ec7d37d434f11fcfb09f9
fixiabis <fixiabis@gmail.com>
2022-05-14 11:44:15 +0800

    重構: 類型

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -9,28 +9,25 @@ const modes = {
   modern: new ModernGameMode(),
 };
 
+type GameMode<TModeName extends GameModeName> = typeof modes[TModeName];
+
+const stateConstructors = {
+  classic: ClassicGameState,
+  modern: ModernGameState,
+};
+
+type GameState<TModeName extends GameModeName> = typeof stateConstructors[TModeName]['prototype'];
+
 export type GameModeName = keyof typeof modes;
 
-type GameMode<TModeName extends GameModeName> = {
-  classic: ClassicGameMode;
-  modern: ModernGameMode;
-}[TModeName];
-
-type GameState<TModeName extends GameModeName> = {
-  classic: ClassicGameState;
-  modern: ModernGameMode;
-}[TModeName];
-
-class Game<TModeName extends GameModeName> {
+class Game<TModeName extends GameModeName = 'modern'> {
   public readonly mode: GameMode<TModeName>;
   public readonly state: GameState<TModeName>;
 
-  constructor(mode: TModeName = 'modern' as TModeName, state: Partial<GameState<TModeName>> = {}) {
+  constructor(mode = 'modern' as TModeName, state: Partial<GameState<TModeName>> = {}) {
+    const GameState = stateConstructors[mode];
     this.mode = modes[mode];
-
-    this.state = (
-      mode === 'classic' ? new ClassicGameState(state) : new ModernGameState(state)
-    ) as GameState<TModeName>;
+    this.state = new GameState(state) as GameState<TModeName>;
   }
 
   public takeMove(move: GameMove) {
