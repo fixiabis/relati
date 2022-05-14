ac81943c2124dd93bbdbacac3f64e485e88efb68
fixiabis <fixiabis@gmail.com>
2022-05-14 13:11:20 +0800

    重構: 型別變數命名

--- src/core/Player.ts
+++ src/core/Player.ts
@@ -2,7 +2,7 @@ import Game, { GameModeName } from './Game';
 
 interface Player<TMode extends GameModeName> {
   onCanMove?(game: Game<TMode>): void;
-  onPlayersEliminated?(game: Game<TMode>, players: readonly number[]): void;
+  onPlayersEliminated?(game: Game<TMode>, eliminatedPlayers: readonly number[]): void;
   onGameEnded?(game: Game<TMode>): void;
 }
 
