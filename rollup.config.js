d5266da5162767cc2fbc238a4d9491e760eff828
fixiabis <fixiabis@gmail.com>
2022-07-23 08:35:03 +0800

    瑣事: 調整設定，使用 jest

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,13 @@
 import typescript from 'rollup-plugin-typescript2';
 
-const defaults = { compilerOptions: { declaration: true } };
-const hideDeclaration = { compilerOptions: { declaration: false } };
-
 export default [
   {
     input: './src/index.ts',
 
-    plugins: [
-      typescript({
-        tsconfig: 'tsconfig.json',
-        tsconfigDefaults: defaults,
-      }),
-    ],
+    plugins: [typescript({ tsconfig: 'tsconfig.json' })],
 
     output: {
-      file: 'lib/index.js',
-      format: 'cjs',
-      indent: true,
-    },
-  },
-  {
-    input: './src/index.ts',
-
-    plugins: [
-      typescript({
-        tsconfig: 'tsconfig.json',
-        tsconfigDefaults: hideDeclaration,
-      }),
-    ],
-
-    output: {
-      file: 'dist/relati.js',
+      file: 'dist/index.js',
       format: 'umd',
       name: 'relati',
       indent: true,
