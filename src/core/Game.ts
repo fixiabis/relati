3d7e8a22b96529923ba01e71d3e38d4912f3d558
fixiabis <fixiabis@gmail.com>
2022-07-26 14:21:11 +0800

    功能: 經典版遊戲

--- src/core/Game.ts
+++ src/core/Game.ts
@@ -1,8 +1,11 @@
 import { Board } from './board/Board';
 import { BoardSquare } from './board/BoardSquare';
+import { DirectionCoordinate } from './coordinates/DirectionCoordinate';
 import { Position, PositionCoordinate } from './coordinates/PositionCoordinate';
-import { Piece } from './Piece';
+import { Piece, PieceSymbol } from './Piece';
 import { Player } from './Player';
+
+const NearbyDirections = ['F', 'B', 'L', 'R', 'FL', 'FR', 'BL', 'BR'].map(DirectionCoordinate.parse);
 
 export interface GameInit {
   board: Board<Piece>;
@@ -12,7 +15,7 @@ export interface GameInit {
   allPlayersHavePlaced?: boolean;
 }
 
-export abstract class Game {
+export class Game {
   public readonly players: readonly Player[];
   public readonly board: Board<Piece>;
   public ended: boolean;
@@ -29,27 +32,74 @@ export abstract class Game {
     this.allPlayersHavePlaced = init.allPlayersHavePlaced || false;
   }
 
-  public placePiece(player: Player, position: Position): void {
-    this.beforePlacePiece(player, position);
+  public placePiece(pieceSymbol: PieceSymbol, position: Position) {
+    if (pieceSymbol !== this.activePlayer.pieceSymbol) {
+      throw new Error('符號非該玩家的回合');
+    }
 
     const positionCoordinate = PositionCoordinate.parse(position);
     const square = this.board.squareAt(positionCoordinate);
-    this.placePieceOnSquare(player, square);
 
-    this.afterPlacePiece(square.piece!);
+    this.placePieceOnSquare(pieceSymbol, square);
+    this.checkAllPlayersHavePlaced();
+    this.nextPlayerTurnOrEnd();
   }
 
-  protected beforePlacePiece(player: Player, _position: Position): void {
-    if (player !== this.activePlayer) {
-      throw new Error('Player not active');
+  private placePieceOnSquare(pieceSymbol: PieceSymbol, square: BoardSquare): void {
+    if (this.allPlayersHavePlaced && !this.anySimilarPieceNearby(square, pieceSymbol)) {
+      throw new Error('無法聯繫到附近的符號');
+    }
+
+    const piece = new Piece(pieceSymbol, square);
+    square.placePiece(piece);
+  }
+
+  private anySimilarPieceNearby(square: BoardSquare, pieceSymbol: PieceSymbol): boolean {
+    return this.findNearbyPieces(square).some((nearbyPiece) => nearbyPiece.symbol === pieceSymbol);
+  }
+
+  private findNearbyPieces(square: BoardSquare): Piece[] {
+    return NearbyDirections.filter((direction) => square.squareDefinedTo(direction))
+      .map((direction) => square.squareTo(direction).piece)
+      .filter(Boolean);
+  }
+
+  private checkAllPlayersHavePlaced(): void {
+    if (!this.allPlayersHavePlaced && this.activePlayer === this.players[this.players.length - 1]) {
+      this.allPlayersHavePlaced = true;
     }
   }
 
-  protected abstract placePieceOnSquare(player: Player, square: BoardSquare<Piece>): void;
+  private nextPlayerTurnOrEnd(): void {
+    const activePlayerIndex = this.players.indexOf(this.activePlayer);
 
-  protected afterPlacePiece(_piece: Piece): void {
-    this.changeNextPlayerOrEnd();
+    const playersAfterActive = this.players
+      .slice(activePlayerIndex + 1)
+      .concat(this.players.slice(0, activePlayerIndex));
+
+    const nextPlayer = this.findNextPlayer(playersAfterActive);
+
+    if (nextPlayer) {
+      this.activePlayer = nextPlayer;
+      return;
+    }
+
+    if (this.anySquarePlayerCanPlace(this.activePlayer)) {
+      this.winner = this.activePlayer;
+    }
+
+    this.ended = true;
   }
 
-  protected abstract changeNextPlayerOrEnd(): void;
+  private findNextPlayer(players: Player[]): Player | null {
+    return players.find((who) => this.anySquarePlayerCanPlace(who)) || null;
+  }
+
+  private anySquarePlayerCanPlace(player: Player): boolean {
+    return this.board.squareList.some((square) => this.squareCanPlace(square, player.pieceSymbol));
+  }
+
+  private squareCanPlace(square: BoardSquare, pieceSymbol: PieceSymbol): boolean {
+    return square.piece === null && (!this.allPlayersHavePlaced || this.anySimilarPieceNearby(square, pieceSymbol));
+  }
 }
